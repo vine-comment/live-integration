d7e80753d959faf32f92db34265c27100322cd53
geekben <bn0418@gmail.com>
2016-06-19 17:16:44 +0800

    bug fix

--- live_portal/settings.py
+++ live_portal/settings.py
@@ -14,9 +14,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'live_portal',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'Intel123',
-        'HOST': '0.0.0.0',        # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'xxxxx',
+        'PASSWORD': 'xxxxx',
+        'HOST': '',        # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
