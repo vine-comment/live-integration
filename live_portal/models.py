d1121b91035884e37562624aa303382dc8066591
geekben <bn0418@gmail.com>
2016-06-19 16:05:01 +0800

    update model and refine html

--- live_portal/models.py
+++ live_portal/models.py
@@ -4,5 +4,7 @@ class Room(models.Model):
     anchor = models.CharField(max_length=100)
     tag = models.CharField(max_length=50)
     room_name = models.CharField(max_length=100)
+    platform_name = models.CharField(max_length=100)
+    creation_time = models.DateTimeField(auto_now=False)
     url = models.CharField(max_length=200)
-    audience_c = models.CharField(max_length=100)
+    audience_c = models.IntegerField(default=0)
