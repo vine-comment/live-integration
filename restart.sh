3dc8f49ed04f405b40b4115fda7586039bddfde0
geekben <bn0418@gmail.com>
2016-07-02 01:03:17 +0800

    update restart script

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
+source /root/vine/bin/activate
 # NOTE: This script will kill all django processes.
 pkill -f 'python manage.py runserver 0.0.0.0:8080'
 export PYTHONIOENCODING=utf-8
 python manage.py runserver 0.0.0.0:8080
 ps aux | grep --color \[m\]anage\.py
-echo "Restart live_portal succeed!"
