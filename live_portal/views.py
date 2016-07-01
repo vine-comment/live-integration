0e07d7bb036c0a666f12e2ae33a6672ae4456bf7
geekben <bn0418@gmail.com>
2016-07-02 00:11:07 +0800

    update video_image min-width to be responsive. remove useless code

--- live_portal/views.py
+++ live_portal/views.py
@@ -20,22 +20,6 @@ class ShowView(TemplateView):
             rooms = Room.objects.filter(modification_time__gte=timezone.now()+timedelta(hours=7))[:100]
             tag = u'所有直播'
         else:
-            if tag == 'yxlm':
-                tag = u'英雄联盟'
-            elif tag == 'zjyx':
-                tag = u'主机游戏'
-            elif tag == 'yqly':
-                tag = u'元气领域'
-            elif tag == 'swxf':
-                tag = u'守望先锋'
-            elif tag == 'dota2':
-                tag = u'DOTA2'
-            elif tag == 'lscs':
-                tag = u'炉石传说'
-            elif tag == 'yllm':
-                tag = u'娱乐联萌'
-            elif tag == 'nt':
-                tag = u'女团'
             rooms = Room.objects.filter(tag=tag, modification_time__gte=timezone.now()+timedelta(hours=7))[:100]
 
         return render(request, self.template_name,
