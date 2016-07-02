0bf04358e9c9b2dd1baf7df28419710ec4ac4fda
geekben <bn0418@gmail.com>
2016-07-02 16:08:58 +0800

    update restart.sh for dev repo

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 source /root/vine/bin/activate
 # NOTE: This script will kill all django processes.
-pkill -f 'python manage.py runserver 0.0.0.0:8080'
+pkill -f 'python manage.py runserver 0.0.0.0:8088'
 export PYTHONIOENCODING=utf-8
-python manage.py runserver 0.0.0.0:8080
+python manage.py runserver 0.0.0.0:8088
 ps aux | grep --color \[m\]anage\.py
