8fd2e3abb9a310fcecb2fc9bb34352a0adc11ba1
geekben <bn0418@gmail.com>
2016-07-02 16:52:22 +0800

    don't track restart.sh

--- restart.sh
+++
@@ -1,6 +0,0 @@
-source /root/vine/bin/activate
-# NOTE: This script will kill all django processes.
-pkill -f 'python manage.py runserver 0.0.0.0:8088'
-export PYTHONIOENCODING=utf-8
-python manage.py runserver 0.0.0.0:8088
-ps aux | grep --color \[m\]anage\.py
